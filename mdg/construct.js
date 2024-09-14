b30b7a2d385453745358211db931168e70914c8e
MoonBirdLin <67228106+MoonBirdLin@users.noreply.github.com>
2024-09-14 22:31:57 +0800

    Module Dependency Graph Analysis Finished

---
+++ mdg/construct.js
@@ -0,0 +1,84 @@
+const fs = require('fs');
+const path = require('path');
+const esprima = require('esprima');
+const astCtrl = require('../scenery/astCtrl');
+const astParserCtrl = require('../scenery/astParserCtrl');
+const { exit } = require('process');
+const { error } = require('console');
+
+async function processModule(ast, currModuleName){
+    let imports = [];
+    let exports = [];
+    let parser = await astParserCtrl.getOrSetAstParser();
+    parser.traverseAST(ast, function (node) {
+        // Find all require statements
+        if (node.type === 'CallExpression' && node.callee.name === 'require') {
+            const moduleName = node.arguments[0].value; // 获取 require 的模块名称
+            imports.push(moduleName);
+        }
+
+        // 查找 module.exports 语句
+        if (node.type === 'AssignmentExpression' &&
+            node.left.object &&
+            node.left.object.name === 'module' &&
+            node.left.property.name === 'exports') {
+                if (node.right.type === 'Identifier') {
+                    exports.push(currModuleName+'$$Variable$$'+node.right.name); // 记录导出的对象
+                } else if (node.right.type === 'FunctionExpression' || node.right.type === 'ArrowFunctionExpression') {
+                    exports.push(currModuleName+'$$function$$'+String(node.right._id)); // 记录导出的函数
+                } else {
+                    exports.push(currModuleName+'$$uncatched_object$$'+String(node.id)); // 记录导出的非函数对象
+                }
+            }
+        }
+    );
+    return [imports, exports];
+}
+
+class ModuleDependencyGraph {
+    constructor() {
+        this.moduleDependencyGraph = new Map();
+        
+    }
+    async initialize() {
+        await this.constructMDG();
+    }
+    async constructMDG(inputAstPool = null) {
+        let astpool = inputAstPool;
+        if (astpool == null) {
+            astpool = astCtrl.getAstPool();
+        }
+        if (astpool.size == 0) {
+            console.error('AST pool is empty');
+            exit(1);
+        }
+        for (let [name, ast] of astpool.getAstPool()) {
+            let [imports, exports] = await processModule(ast, name);
+            this.moduleDependencyGraph.set(name, {imports, exports});
+        }
+        // console.log('MDG constructed');
+    }
+    
+    dumpMDG() {
+        let mapObj = Object.fromEntries(this.moduleDependencyGraph);
+        return JSON.stringify(mapObj, null, 4)
+    }
+    
+}
+
+function getMDG() {
+    var delegate = null;
+    function mdgInstance () {
+        if (delegate == null) {
+            delegate = new ModuleDependencyGraph();
+        }
+        return delegate;
+    }
+    return mdgInstance;
+}
+
+let moduleDependencyGraphSingleTon = getMDG();
+
+module.exports = {
+    getMDG: moduleDependencyGraphSingleTon,
+};
